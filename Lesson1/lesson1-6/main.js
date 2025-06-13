73d56d136ce5b6359d1e4060615b7cc56300a847
Ann Kravets <kravetsanna78@gmail.com>
2025-06-13 16:32:30 +0300

    create  lesson1-6

---
+++ Lesson1/lesson1-6/main.js
@@ -0,0 +1,23 @@
+// console.log(typeof (100500 + ''));
+// console.log(100 + 200 + '');
+// console.log('' + 100 + 200);
+
+// console.log(+'100500');
+// console.log(parseInt('100500asd123'));
+// console.log(parseFloat('100500.123'));
+
+console.log(!!1000);
+console.log(!!0);
+console.log(!!'asd');
+console.log(!!''); //false
+
+console.log(!!'0'); //true
+
+console.log(true + ''); //true
+console.log(+true); //1
+console.log(+false); //0
+
+console.log(100 + true); //101
+console.log(100 - true); //99
+
+
