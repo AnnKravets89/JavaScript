b6e9cc7e6d22ece9b25ecf0aa93907d2e5b61c1e
Ann Kravets <kravetsanna78@gmail.com>
2025-06-13 15:32:14 +0300

    create intro lesson1-4

---
+++ Lesson1/lesson1-4/main.js
@@ -0,0 +1,23 @@
+let a = 100500; //integer
+console.log(a);
+
+let b = 3.123456; //float double
+console.log(b);
+
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(10 / 20);
+console.log(11 % 3);
+
+console.log(0.1 + 0.2);
+
+let big = 123456789023456789n;
+console.log(big);
+let BigInt = 123456789023456789;
+console.log(BigInt);
+
+let n1 = 100;
+let n2 = 200;
+console.log(n1 + n2);
+
