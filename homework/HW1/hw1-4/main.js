6844df40f5cc062ee8bf03e9a54933401d245ba0
Ann Kravets <kravetsanna78@gmail.com>
2025-06-13 18:24:54 +0300

    create hw1-4.html main.js

---
+++ homework/HW1/hw1-4/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt('What is your name?');
+let middleName = prompt('What is your middle name?');
+let age = prompt('How old are you?');
+
+let result = `${firstName} ${middleName} ${age}`;
+console.log(result);
+
