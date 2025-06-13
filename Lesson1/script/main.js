e8ef8b98b122abcd8a37f8dcc6a851f8800c646c
Ann Kravets <kravetsanna78@gmail.com>
2025-06-13 13:15:24 +0300

    create intro lesson1-1

---
+++ Lesson1/script/main.js
@@ -0,0 +1,25 @@
+console.log('djcuvvshjk');
+
+// number
+// string
+// boolean
+
+// var a;
+// a = 'hey okten';
+
+// {
+//     let a = 'hey okten';
+//     var b = 'hey world';
+// }
+// console.log(b);
+// console.log(a);
+
+const PI = 3.14;
+console.log(PI);
+
+
+let a = 'fghjk';
+console.log(a);
+a = 'hello';
+console.log(a);
+
