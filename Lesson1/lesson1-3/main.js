e4950cbc789eba0d78a1b069f57d77f1045f8fb4
Ann Kravets <kravetsanna78@gmail.com>
2025-06-13 13:48:06 +0300

    create intro lesson1-3

---
+++ Lesson1/lesson1-3/main.js
@@ -0,0 +1,14 @@
+// let a = "'Anna'";
+// let b = '"Anna"';
+// console.log(a, b);
+//
+// let c = `okten`;
+// console.log(c);
+
+let greeting = 'Hello ';
+let intro = 'my name is Ann';
+let msg = greeting + intro;
+console.log(msg);
+
+let msg2 = `${greeting} ${intro}`;
+console.log(msg2);
