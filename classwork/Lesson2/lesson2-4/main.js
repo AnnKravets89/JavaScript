b4ca976cd887ef07fb576ebace41300e6a721333
Ann Kravets <kravetsanna78@gmail.com>
2025-06-14 12:35:43 +0300

    create lesson2-4.html main.js

---
+++ classwork/Lesson2/lesson2-4/main.js
@@ -0,0 +1,19 @@
+let user1 = {
+    id : 1,
+    name : "Anna",
+    age : 35,
+    skills : ['html','css','javascript'],
+    husbend : {
+        name : 'Sergey',
+        age : 38
+    }
+};
+
+// console.log(user1);
+// console.log(user1.id);
+//
+// console.log(user1['age']);
+// console.log(user1.skills[1]);
+
+console.log(user1.husbend);
+console.log(user1.husbend.name);
