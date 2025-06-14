49ccfc3c5bd01c940369a37ce29b3535d3d3ef6d
Ann Kravets <kravetsanna78@gmail.com>
2025-06-14 13:40:23 +0300

    create lesson2-7.html main.js

---
+++ classwork/Lesson2/lesson2-7/main.js
@@ -0,0 +1,15 @@
+// number
+// string
+// boolean
+
+let a = 100;
+let b = a;
+b = b + 10;
+console.log(a);
+
+let user = { name : 'vasya'};
+let user2 = user;
+console.log(user2);
+user2.age = 30;
+console.log(user2);
+console.log(user);
