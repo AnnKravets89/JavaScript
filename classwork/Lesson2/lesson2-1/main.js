cf96051d55d4530722d20a37ea2f5759bd900995
Ann Kravets <kravetsanna78@gmail.com>
2025-06-14 12:02:14 +0300

    create hw2-1.html main.js

---
+++ classwork/Lesson2/lesson2-1/main.js
@@ -0,0 +1,17 @@
+let arr;
+arr = [123, 234,765, -456, 0, true, 'fghjk'];
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[4]);
+console.log(arr[3]);
+console.log(arr.length);
+
+arr[0] = 'new value';
+console.log(arr);
+
+arr[7] = 'okten';
+console.log(arr);
+
+arr[arr.length] = 45677;
+console.log(arr);
+
