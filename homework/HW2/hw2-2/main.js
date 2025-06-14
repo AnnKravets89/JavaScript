0904702233645889e833470844ddc1dde6e93d25
Ann Kravets <kravetsanna78@gmail.com>
2025-06-14 15:44:25 +0300

    add hw2-2.html main.js

---
+++ homework/HW2/hw2-2/main.js
@@ -0,0 +1,20 @@
+let book1 = {
+    title : '1984',
+    pageCount : 328,
+    genre : 'Dystopian fiction'
+};
+
+let book2 = {
+    title : 'Pride and Prejudice',
+    pageCount : 279,
+    genre : 'Classic romance'
+};
+
+let book3 = {
+    title : 'The Martian',
+    pageCount : 369,
+    genre : 'Science fiction'
+};
+console.log(book1);
+console.log(book2);
+console.log(book3);
