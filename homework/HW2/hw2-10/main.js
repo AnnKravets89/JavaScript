3d0249b524ec5ab4b496d4b705635a6ad0da8f5e
Ann Kravets <kravetsanna78@gmail.com>
2025-06-16 12:21:17 +0300

    add hw2-10.html main.js

---
+++ homework/HW2/hw2-10/main.js
@@ -0,0 +1,10 @@
+let x  = +prompt('Enter first number');
+let y = +prompt('Enter second number');
+
+if (x > y) {
+    console.log(x);
+} else if (x < y) {
+    console.log(y);
+} else if (x === y){
+    console.log('numbers are equal');
+}
