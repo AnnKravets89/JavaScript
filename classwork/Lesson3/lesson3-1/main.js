a775542e43e1b81ea9d661fbde037c51b49e2d2a
Ann Kravets <kravetsanna78@gmail.com>
2025-06-16 13:40:01 +0300

    add lesson3-1.html main.js

---
+++ classwork/Lesson3/lesson3-1/main.js
@@ -0,0 +1,25 @@
+// let x = 0;
+// x = x + 1; // x = 1
+// console.log(x);
+// x++;
+// console.log(x); // x = 2
+// x  += 1;
+// console.log(x); // 3
+//
+// x = x -1;
+// console.log(x); // 2
+// x -= 1;
+// console.log(x); // 1
+// x--;
+// console.log(x); // 0
+
+// let x = 0;
+// x++;
+
+
+let a = 0;
+let b = a++;
+console.log(a, b); // a = 1; b = 0;
+
+let b = ++a;
+console.log(a, b); // a = 1 b = 1
