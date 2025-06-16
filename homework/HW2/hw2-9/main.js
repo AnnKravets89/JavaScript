54d61c1c8773128c0e7628067a6a11f17390039a
Ann Kravets <kravetsanna78@gmail.com>
2025-06-16 12:02:35 +0300

    add hw2-9.html main.js

---
+++ homework/HW2/hw2-9/main.js
@@ -0,0 +1,27 @@
+let day = 7;
+
+switch  (day) {
+    case 1:
+        console.log('Team meeting at 10:00 AM');
+    break;
+    case 2:
+        console.log('Gym workout in the evening');
+        break;
+    case 3:
+        console.log('Grocery shopping after work');
+        break;
+    case 4:
+        console.log('Online English class at 7:00 PM');
+        break;
+    case 5:
+        console.log('Dinner with friends');
+        break;
+    case 6:
+        console.log('Day trip to the countryside');
+        break;
+    case 7:
+        console.log('Relax and read a book');
+        break;
+    default:
+        console.log('error');
+}
