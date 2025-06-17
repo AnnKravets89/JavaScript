798b630f2607e6c88f949e97d19f01c0f16fce30
Ann Kravets <kravetsanna78@gmail.com>
2025-06-17 13:32:54 +0300

    add extra-hw3-2.html main.js

---
+++ homework/HW3/Extra Tasks/extra-hw3-2/main.js
@@ -0,0 +1,13 @@
+let arr = [];
+
+arr[0] = 57;
+arr[1] = 'tomato';
+arr[2] = true;
+arr[3] = null;
+arr[4] = undefined;
+arr[5] = NaN;
+arr[6] = {
+    name: 'Ann',
+    age: 35,
+};
+console.log(arr);
