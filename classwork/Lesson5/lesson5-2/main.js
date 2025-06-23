f40754c6adef4ed72e2aa6c02d58dd1917d99e0f
Ann Kravets <kravetsanna78@gmail.com>
2025-06-23 12:48:47 +0300

    add lesson5-2.html main.js

---
+++ classwork/Lesson5/lesson5-2/main.js
@@ -0,0 +1,13 @@
+// function calc(a,b) {
+//     return a + b;
+// }
+// console.log(calc(5,8));
+
+let calc = (a, b) => a + b;
+console.log(calc(5,5));
+
+let asd = function () {
+    console.log('Hi');
+    return undefined;
+}
+console.log(asd());
