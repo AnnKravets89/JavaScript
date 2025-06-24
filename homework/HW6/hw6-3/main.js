a48758ddcc3f0971d7cd9750438e1bb54b5492e3
Ann Kravets <kravetsanna78@gmail.com>
2025-06-24 17:39:55 +0300

    add hw6-3.html main.js

---
+++ homework/HW6/hw6-3/main.js
@@ -0,0 +1,8 @@
+function lowCaseStr(strings) {
+    for (const s of strings) {
+        console.log(s.toLowerCase());
+    }
+}
+lowCaseStr(['HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL']);
+
+
