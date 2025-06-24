7872402a55af6b637b701278c4bbffa855c16381
Ann Kravets <kravetsanna78@gmail.com>
2025-06-24 12:31:57 +0300

    add lesson6-1.html main.js

---
+++ classwork/Lesson6/lesson6-1/main.js
@@ -0,0 +1,25 @@
+let str = 'Hello world!';
+// console.log(str);
+// console.log(str[0]);
+
+// for (const strElement of str) {
+//     console.log(strElement);
+// }
+
+// let s = new String('anything');
+// console.log(typeof s);
+
+// let s = str.concat('!!!!!!');
+// console.log(s);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.startsWith('hel'));
+console.log(str.endsWith('ld!'));
+console.log(str.substring(0, 7));
+console.log(str.indexOf('H'));
+console.log(str.lastIndexOf('d'));
+console.log(str.charAt(8));
+console.log(str.replace('o', '@'));
+console.log(str.split(' '));
+// console.log(str.replaceAll('o', '@'));
