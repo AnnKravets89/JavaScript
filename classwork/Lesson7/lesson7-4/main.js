b3ec2ffcf9cd6b50799ac314601a98d7b70ade63
Ann Kravets <kravetsanna78@gmail.com>
2025-06-26 15:08:29 +0300

    add lesson7-4.html main.js

---
+++ classwork/Lesson7/lesson7-4/main.js
@@ -0,0 +1,24 @@
+// let now = new Date();
+// console.log(now);
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+// console.log(now.getMilliseconds());
+// console.log(now.getTime());
+
+// let date1 = new Date(1750939037471);
+// console.log(date1);
+
+// let date1 = new Date("June 10 1991 12:30:34");
+// let date1 = new Date(1987, 0, 30, 12, 30, 50, 0);
+
+let user = {
+    id: 1,
+    username: 'kokos',
+    bday: new Date(1750939037471)
+}
+console.log(user);
+
